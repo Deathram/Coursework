4bf445499632e0770b8122c2b68c920cac278a59
Deathram <orvavusqa@gmail.com>
2021-11-14 19:28:52 +0000

    minor update

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -31,7 +31,7 @@ class Dec2Hex
 
         else
         {
-            System.out.println("Please input something into the field and don't leave it empty");
+            System.out.println("Please input something into the field and don't leave it empty" + e);
         }
 
         }
